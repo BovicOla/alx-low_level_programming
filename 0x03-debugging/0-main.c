1637749dc7c5d212fab5c637226933ba0ae47929
Bode Olaleye <you@example.com>
2022-07-07 16:29:36 -0700

    Test for positive integer

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,13 +10,8 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
